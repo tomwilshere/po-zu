8fce29b4a6d1188f9d505914694f00004e884343
Tom Wilshere <tom.wilshere@gmail.com>
2013-07-26 08:33:58 +0100

    Zoomy shoe.

--- app/assets/javascripts/intro.js
+++ app/assets/javascripts/intro.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	$('.navbar').css("top", "-40px");
 	$('.background').fadeIn(1000);
-	$('.hero-shoe').delay(700).fadeTo(1000,1);
+	$('.hero-shoe').show();
+	var shoeWidth = $('.hero-shoe img').width();
+	$('.hero-shoe').hide().delay(700).fadeTo(1000,1);
+	$('.hero-shoe img').css("width", "0").delay(700).animate({width: shoeWidth}, 1000);
 	$('.navbar').delay(1000).animate({top: "0"}, 300);
 });
